346d6532b734b4bc0e020fd750cdbe996ac7a42e
yoshocon <ok841001@gmail.com>
2020-02-04 17:28:38 +0800

    [更新] 將Header固定位置及加入Icon

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template lang="pug">
   #app
     .header
+      .logo
+        img(alt='Vue logo' src='./assets/mumi.png' width="30px" heigth="30px")
       .option MUMI001
       .option MUMI002
       .option MUMI003
